cb3ab0458383f7ecaacf117876d432125bc0b773
Edmon Luan <edmonluan@gmail.com>
2016-06-03 20:45:23 -0300

    upgrade maven dependencies

--- src/test/java/org/ruleml/translation/ruleml2tptp/TranslatorTest.java
+++ src/test/java/org/ruleml/translation/ruleml2tptp/TranslatorTest.java
@@ -1,33 +1,26 @@
 package org.ruleml.translation.ruleml2tptp;
 
-import static org.apache.commons.io.FileUtils.*;
-import static org.junit.Assert.*;
-
+import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.io.StringWriter;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.sax.SAXTransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import org.junit.Test;
+import static org.apache.commons.io.FileUtils.*;
+import static org.junit.Assert.*;
 import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
-import org.junit.runner.RunWith;
 
 /**
  * @author edmonluan@gmail.com (Meng Luan)
  */
 @RunWith(Parameterized.class)
-public class TranslatorTest
-{
+public class TranslatorTest {
 
     private static Translator translator;
 
@@ -37,17 +30,17 @@ public class TranslatorTest
         translator.loadTemplates();
     }
 
-    @Parameters(name="{index}: {0}")
-    public static Object[] baseNames() {
-        return new Object[] {
+    @Parameters(name = "{index}: {0}")
+    public static String[] baseNames() {
+        return new String[]{
             "Atom", "Implies", "Forall", "Exists", "Equal", "And", "Or", "Expr",
             "RelFunIndVar", "comments", "Equivalent", "Neg", "iri"
         };
     }
 
-    private String baseName;
+    private final String baseName;
 
-    public TranslatorTest(String baseName) {
+    public TranslatorTest(final String baseName) {
         this.baseName = baseName;
     }
 
@@ -57,12 +50,11 @@ public class TranslatorTest
         final File result = File.createTempFile("TranslatorTest_" + baseName + "Test_", ".tptp.tmp");
         translator.translate(new StreamSource(input), new StreamResult(result));
         final String expected = readFileToString(
-                new File(getClass().getResource("/TranslatorTest/" + baseName + "Test.tptp").toURI()),
-                StandardCharsets.UTF_8);
+            new File(getClass().getResource("/TranslatorTest/" + baseName + "Test.tptp").toURI()),
+            StandardCharsets.UTF_8);
         final String resultFilePath = result.getCanonicalPath();
         final String resultContent = readFileToString(result, StandardCharsets.UTF_8);
         assertEquals("Result file: " + resultFilePath, expected, resultContent);
         assertTrue("Failed to delete " + resultFilePath, result.delete());
     }
-
 }
